a66ce9f112a75c74626e8eaa8d0f6c6340be3f1c
Gingka/Ginger Pepper <gingkagingerpepper@gmail.com>
2022-10-22 16:13:34 -0400

    add functioning scripts

---
+++ Cryptoprice.js
@@ -0,0 +1,136 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: blue; icon-glyph: dollar-sign;
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies
+// or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+// AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+//// I dont know if i need this notice, as i've rewritten most of the code to bring it up to date,
+//// as well as added functionality. Oh well!
+/**
+ * Displays cryptocurrency price information from CoinGecko.
+ * Modified from the featured "Crypto Price" widget.
+ * Name: Cryptoprice
+ * Author: Ging
+ * Year: 2022
+*/
+const PRESENT_SIZE = "Small"
+// Widget setup
+const common = importModule('ging-common')
+
+const params = args.widgetParameter ? args.widgetParameter.split(',') : ['bitcoin', 'ethereum']
+const env = config
+
+
+// Select file source
+const files = common.isIniCloud(FileManager.local(), module.filename) ? FileManager.iCloud() : FileManager.local()
+const bgPath = common.getFile(files, '/rice/topleft.jpg')
+
+// Self-update
+if (!env.widgetFamily) {
+    // check file update date
+    const UPDATE_PERIOD = 30 // days
+    const lastUpdated = files.modificationDate(module.filename)
+    if (common.determineDaysFromNow(lastUpdated) >= UPDATE_PERIOD) {
+        // Update
+        (await common.selfUpdate({
+            srcurl: 'https://gist.github.com/ExperiBass/919f9f2f9676d1e9c4a63860a71657a3/raw/31ee5cda2bdaf752cb528510a2f2c9f9c78b0d26/Cryptoprice.js',
+            filename: module.filename, fs: files, shouldPiggyback: true
+        }))
+    }
+}
+
+const widget = new ListWidget()
+widget.backgroundImage = files.readImage(bgPath)
+
+async function buildWidget() {
+    for (const coin of params) {
+        try {
+            await addCryptoLine(coin)
+        } catch (e) {
+            console.error(e)
+        }
+    }
+}
+
+async function addCryptoLine(name) {
+    const {
+        image,
+        symbol,
+        price,
+        grow,
+        growPercent,
+        id
+    } = await fetchCoinInfo(name)
+
+    const rowStack = common.createStack({
+        parent: widget, padding: [2, 2, 0, 0],
+        align: 'center', verticalLayout: false
+    })
+
+    if (env.widgetFamily && env.widgetFamily !== "small") {
+        rowStack.url = `https://www.coingecko.com/en/coins/${id}`
+        const imageStack = common.createStack({ parent: rowStack, padding: [0, 0, 0, 5] })
+        common.createImage({
+            parent: imageStack,
+            image: await common.loadImage(image),
+            width: 20, height: 20, align: 'left'
+        })
+    }
+    const symbolStack = common.createStack({ parent: rowStack, padding: [0, 0, 0, 5] })
+    rowStack.addSpacer()
+    const priceStack = common.createStack({ parent: rowStack, padding: [0, 0, 0, 0] })
+
+
+    // The text
+    const symbolText = symbolStack.addText(symbol)
+    symbolText.font = Font.mediumSystemFont(16)
+    symbolText.leftAlignText()
+    const priceText = priceStack.addText(price)
+    priceText.font = Font.mediumSystemFont(16)
+    priceText.rightAlignText()
+
+    symbolText.textColor = new Color('#FFFFFF')
+
+    if (env.widgetFamily && env.widgetFamily !== "small") {
+        const percentStack = common.createStack({ parent: rowStack, padding: [0, 0, 8, 0] })
+        const percentText = percentStack.addText(growPercent)
+        if (grow) {
+            percentText.textColor = new Color('#4AF956')
+            percentText.text = `+${percentText.text}`
+        } else {
+            percentText.textColor = new Color('#FD4E00')
+        }
+    }
+
+}
+
+async function fetchCoinInfo(coinID) {
+    const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coinID}`
+
+    const req = new Request(url)
+    const apiResult = (await req.loadJSON())[0]
+    return {
+        price: common.formatNumber(apiResult.current_price),
+        grow: (apiResult.price_change_24h > 0),
+        growPercent: `${common.formatNumber(apiResult.price_change_percentage_24h, { style: 'percent', maximumFractionDigits: 2 })}`,
+        symbol: apiResult.symbol.toUpperCase(),
+        image: apiResult.image, id: apiResult.id
+    }
+}
+
+await buildWidget()
+
+Script.setWidget(widget)
+Script.complete()
+widget[`present${PRESENT_SIZE}`]()
---
+++ ging-common.js
@@ -0,0 +1,215 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: light-gray; icon-glyph: magic;
+/**
+ * Common methods shared by scripts from Ging.
+ * Feel free to use in your own scripts :3
+ * Author: Ging
+ * Year: 2022
+ * Donate: bc1qd65n4y562q6vdp5lx7953uqj0hqxn9k8rqx08y
+*/
+const SRC_URL = 'https://gist.github.com/ExperiBass/0e619152121c309b850783a9bf5bb2e9/raw/5284534b294ce7de490d55fa6a270019aa7c4c28/ging-common.js'
+
+module.exports = {
+    /**
+     * Method to update your scripts. All it does is update, you'll have to implement your own update cycle code.
+     * An example is at the bottom of this file.
+     * @param {string} filename The name of the file to update. You should pass `module.filename`.
+     * @param {string} srcurl URL to the source code. Needs to be plaintext.
+     * @param {FileManager} fs The FileManager for your script.
+     * @param {boolean} shouldPiggyback Update this helper script! Set to `true` if you want us to piggyback off your updating. Defaults to false.
+     * @param {*} alertOptions Options to pass to createAlert.
+     * @param {string} alertOptions.title
+     * @param {string} alertOptions.message
+     * @param {string} alertOptions.failMessage
+     * @returns {boolean} true if success, false otherwise.
+     */
+    async selfUpdate({ filename, srcurl, fs, shouldPiggyback = false, alertOptions = {
+        title: 'Update complete!',
+        message: 'The script has been updated!', failMessage: 'The script failed to update! D:'
+    } }) {
+        try {
+            const req = new Request(srcurl)
+            fs.writeString(filename, await req.loadString())
+
+            if (shouldPiggyback) {
+                // piggyback off of the end user and update ourselves too
+                const selfup = new Request(SRC_URL)
+                fs.writeString(module.filename, await selfup.loadString())
+            }
+
+            const alert = await module.exports.createAlert(alertOptions.title, alertOptions.message, {
+                actions: [{
+                    type: 'default',
+                    title: "OK"
+                }]
+            })
+            alert.presentAlert()
+            return true
+        } catch (e) {
+            console.error(`[ging-common][selfUpdate]: ${e}`)
+            const alert = await module.exports.createAlert(alertOptions.title, alertOptions.failMessage, {
+                actions: [{
+                    type: 'default',
+                    title: "OK"
+                }]
+            })
+            alert.presentAlert()
+            return false
+        }
+    },
+    /**
+     * CreateAlert
+     * @param {*} title - Title of the alert.
+     * @param {*} msg - Message of the alert.
+     * @param {*} options
+     * @param {[any]} options.actions - Actions to add to the alert. An action object is formatted like this:
+     * {
+     *    "type": "default" // corresponds to the Alert methods. Other values: 'cancel', 'destructive', 'secure', 'text'
+     *    "title": "FooBar" // title displayed on the action
+     *    "text": "oh" // used with type = 'secure'||'text', this is the default text in the field.
+     *    "placeholderText": "owo" // used alongside "text", this is the placeholder text displayed.
+     * }
+     * @returns {Alert} - An Alert object.
+     */
+    async createAlert(title, msg, options = { actions: [] }) {
+        const alrt = new Alert()
+        alrt.title = title
+        alrt.message = msg
+
+        for (const action of options.actions) {
+            switch (action.type) {
+                case 'cancel': {
+                    alrt.addCancelAction(action.title)
+                    break
+                }
+                case 'destructive': {
+                    alrt.addDestructiveAction(action.title)
+                    break
+                }
+                case 'secure': {
+                    alrt.addSecureTextField(action.placeholderText, action.text)
+                    break
+                }
+                case 'text': {
+                    alrt.addTextField(action.placeholderText, action.text)
+                    break
+                }
+                default: {
+                    alrt.addAction(action.title)
+                }
+            }
+        }
+        return alrt
+    },
+    isIniCloud(fs, file) {
+        return fs.isFileStoredIniCloud(file)
+    },
+    formatNumber(number, options) {
+        return new Intl.NumberFormat(undefined, options).format(number)
+    },
+    getFile(fs, path) {
+        return fs.joinPath(fs.documentsDirectory(), path)
+    },
+    async loadImage(imgUrl) {
+        const req = new Request(imgUrl)
+        return await req.loadImage()
+    },
+    getSymbol(name, font) {
+        const symbol = SFSymbol.named(name)
+        if (font) {
+            symbol.applyFont(font)
+        }
+        return symbol
+    },
+    createStack({
+        parent = null,
+        width = 0, // px
+        height = 0, // px
+        backgroundColor = null, // hexadecimal notation
+        borderColor = "#000000",
+        borderRadius = 4, // px
+        borderWidth = 0, // px
+        verticalLayout = false,
+        padding = [0, 0, 0, 0], // array of 4 numbers
+        font = null,
+        align = 'top' // 'top', 'center', 'bottom'
+    }) {
+        try {
+            if (!parent) {
+                throw Error('parent not defined')
+            }
+            const stacc = parent.addStack()
+
+            stacc.size = new Size(width, height)
+            stacc.borderWidth = borderWidth
+            stacc.borderColor = new Color(borderColor)
+            stacc.cornerRadius = borderRadius
+
+            if (backgroundColor) {
+                stacc.backgroundColor = new Color(backgroundColor)
+            }
+            if (verticalLayout) {
+                stacc.layoutVertically()
+            } else {
+                stacc.layoutHorizontally()
+            }
+            if (font) {
+                stacc.font = font
+            }
+            stacc[`${align.toLowerCase()}AlignContent`]()
+            stacc.setPadding(...padding)
+
+            return stacc
+        } catch (e) {
+            throw Error(`[ging-common][createStack]: ${e}`)
+        }
+    },
+    createImage({
+        parent = null,
+        image = null,
+        width = 0,
+        height = 0,
+        resizable = true,
+        color = null,
+        align = 'left' // 'left', 'center', 'right'
+    }) {
+        try {
+            if (!parent) {
+                throw Error('parent not defined')
+            }
+            const img = parent.addImage(image)
+            img.imageSize = new Size(width, height)
+            img.resizable = resizable
+            if (color) {
+                img.tintColor = new Color(color)
+            }
+            img[`${align.toLowerCase()}AlignImage`]()
+            return img
+        } catch (e) {
+            throw Error(`[ging-common][createImage]: ${e}`)
+        }
+    },
+    determineDaysFromNow(date) {
+        const msInDay = 24 * 60 * 60 * 1000
+        const now = new Date()
+        now.setHours(0, 0, 0, 0)
+        date.setHours(0, 0, 0, 0)
+
+        return (+now - +date) / msInDay
+    }
+}
+
+const files = module.exports.isIniCloud(FileManager.local(), module.filename) ? FileManager.iCloud() : FileManager.local()
+// Update if called
+module.exports.selfUpdate({
+    filename: module.filename,
+    srcurl: SRC_URL,
+    fs: files,
+    shouldPiggyback: false, // Dont double-update x3
+    alertOptions: {
+        title: 'Woah, so fancy!',
+        message: `${module.filename} has sucessfully updated!`,
+        failMessage: `${module.filename} failed to update! The error is in the console.`
+    }
+})
